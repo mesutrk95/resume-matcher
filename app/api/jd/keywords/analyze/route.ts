74cf1dda038b2e45a329cfdd914170f6ace4f962
Masoud Rezakhanlou <masoud.rk95@gmail.com>
2025-03-06 21:00:49 +0400

    keyword analyze api fix

--- app/api/jd/keywords/analyze/route.ts
+++ app/api/jd/keywords/analyze/route.ts
@@ -7,8 +7,8 @@ import { getAIJsonResponse } from '@/lib/ai';
 
 // POST /jd/keywords/analyze
 export const POST = withErrorHandling(async (request: Request) => {
-    // const body = await request.json();
-    // const { description } = body; 
+    const body = await request.json();
+    const { description } = body;
 
     // const prompt = `Give me the important keywords in this job description
     //  and give each one a skill type (hard/soft/none) and level of importance(between 0-1) in this format [{ "keyword" : "something", "skill": "hard", "level": 0.55 },...], give me pure json data as normal text no need to format it.` ;
@@ -19,7 +19,7 @@ Additionally, assign a level of importance to each keyword on a scale from 0 to 
 Provide the results in a structured JSON format as an array of objects, where each object contains the fields "keyword", "skill", and "level". 
 Ensure the response is in a valid JSON format with no extra text, without any additional formatting or explanations. catch whatever is important for ATSs, Here is the job:`
 
-    const result = await getAIJsonResponse(prompt, [])
+    const result = await getAIJsonResponse(prompt, [description])
 
     return NextResponse.json(
         { prompt, ...result },
