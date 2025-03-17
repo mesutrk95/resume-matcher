5a69809f5daaf993ff3c0071fa83657fead90d4a
Masoud Rezakhanlou <masoud.rk95@gmail.com>
2025-03-17 14:47:13 +0400

    fix jd preview

--- components/jobs/job-description-preview.tsx
+++ components/jobs/job-description-preview.tsx
@@ -1,27 +1,15 @@
 "use client";
 
-import { analyzeJobByAI } from "@/actions/job";
 import { Job } from "@prisma/client";
-import React, { useTransition } from "react";
-import { toast } from "sonner";
-import moment from "moment";
-import { LoadingButton } from "../ui/loading-button";
-import { Briefcase, LucideExternalLink } from "lucide-react";
+import React from "react";
 
-export const JobDescriptionPreview = ({
-  job,
-  onJobUpdated,
-}: {
-  job: Job;
-  onJobUpdated?: (j: Job) => void;
-}) => {
- 
+export const JobDescriptionPreview = ({ job }: { job: Job }) => {
   return (
     <div className="md:col-span-2 bg-white rounded-lg  ">
-      <div className="space-y-4"> 
+      <div className="space-y-4">
         <h3 className="text-xl font-bold">Job Description</h3>
         <div
-          className="prose prose-sm max-w-none"
+          className="prose prose-sm max-w-none jd-preview"
           dangerouslySetInnerHTML={{ __html: job.description || "" }}
         ></div>
       </div>
