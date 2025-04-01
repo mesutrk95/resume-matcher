044fb8d7881df04bf8e1433b32992e92b363eefd
Milad Bonakdar <miladbonak@gmail.com>
2025-04-01 19:22:56 +0400

    Refactor string quotes to single quotes for consistency and improve error messages in registration and verification processes

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,11 @@
-"use server";
+'use server';
 
-import { registerSchema } from "@/schemas";
-import { z } from "zod";
-import { createUser, getUserByEmail } from "@/services/user";
-import { generateVerificationToken } from "@/services/verification-token";
-import { sendVerificationEmail } from "@/services/mail";
-import { hashPassword, response } from "@/lib/utils";
+import { registerSchema } from '@/schemas';
+import { z } from 'zod';
+import { createUser, getUserByEmail } from '@/services/user';
+import { generateVerificationToken } from '@/services/verification-token';
+import { sendVerificationEmail } from '@/services/mail';
+import { hashPassword, response } from '@/lib/utils';
 
 export const register = async (payload: z.infer<typeof registerSchema>) => {
   // Check if user input is not valid.
@@ -15,7 +15,7 @@ export const register = async (payload: z.infer<typeof registerSchema>) => {
       success: false,
       error: {
         code: 422,
-        message: "Invalid fields.",
+        message: 'Invalid fields.',
       },
     });
   }
@@ -28,7 +28,7 @@ export const register = async (payload: z.infer<typeof registerSchema>) => {
       success: false,
       error: {
         code: 422,
-        message: "Email address already exists. Please use another one.",
+        message: 'Email address already exists. Please use another one.',
       },
     });
   }
@@ -41,12 +41,16 @@ export const register = async (payload: z.infer<typeof registerSchema>) => {
 
   // Generate verification token, then send it to the email.
   const verificationToken = await generateVerificationToken(email);
-  await sendVerificationEmail(verificationToken.email, verificationToken.token);
+  await sendVerificationEmail(
+    verificationToken.email,
+    verificationToken.token,
+    name,
+  );
 
   // Return response success.
   return response({
     success: true,
     code: 201,
-    message: "Confirmation email sent. Please check your email.",
+    message: 'Confirmation email sent. Please check your email.',
   });
 };
