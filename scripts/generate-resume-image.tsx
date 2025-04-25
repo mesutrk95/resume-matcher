dae63f36aa8afd94c8652e3ab873dd6f495b9995
Masoud Rezakhanlou <masoud.rk95@gmail.com>
2025-04-25 15:31:04 +0400

    pdf to image commented

--- scripts/generate-resume-image.tsx
+++ scripts/generate-resume-image.tsx
@@ -9,61 +9,61 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { ResumeContent } from '@/types/resume';
 
-async function pdf2png(pdfFilePath: string) {
-  const { pdf } = await import('pdf-to-img');
-  const document = await pdf(pdfFilePath, { scale: 1 });
-  let counter = 1;
-  for await (const image of document) {
-    const imagePath = path.join(
-      path.dirname(pdfFilePath),
-      path.basename(pdfFilePath, path.extname(pdfFilePath)) + '.png',
-    );
-    await fs.writeFile(imagePath, image, () => {});
-    counter++;
-    break;
-  }
-}
+// async function pdf2png(pdfFilePath: string) {
+//   const { pdf } = await import('pdf-to-img');
+//   const document = await pdf(pdfFilePath, { scale: 1 });
+//   let counter = 1;
+//   for await (const image of document) {
+//     const imagePath = path.join(
+//       path.dirname(pdfFilePath),
+//       path.basename(pdfFilePath, path.extname(pdfFilePath)) + '.png',
+//     );
+//     await fs.writeFile(imagePath, image, () => {});
+//     counter++;
+//     break;
+//   }
+// }
 
-async function processFile(filePath: string): Promise<void> {
-  try {
-    console.log(`Processing file: ${filePath}`);
+// async function processFile(filePath: string): Promise<void> {
+//   try {
+//     console.log(`Processing file: ${filePath}`);
 
-    // Read file content
-    const fileContent = fs.readFileSync(filePath, 'utf8');
+//     // Read file content
+//     const fileContent = fs.readFileSync(filePath, 'utf8');
 
-    // Parse JSON to get the resume content
-    const resumeContent: ResumeContent = JSON.parse(fileContent);
+//     // Parse JSON to get the resume content
+//     const resumeContent: ResumeContent = JSON.parse(fileContent);
 
-    // // Migrate the resume content
-    // const migratedContent = migrateResumeContent(resumeContent);
-    const basePath = path.join(__dirname, '../');
-    const pdfFilePath = path.join(
-      basePath,
-      path.dirname(filePath),
-      path.basename(filePath, path.extname(filePath)) + '.pdf',
-    );
-    await renderToFile(
-      <ResumeDocument resume={resumeContent} resumeDesign={DEFAULT_RESUME_DESIGN} />,
-      pdfFilePath,
-    );
+//     // // Migrate the resume content
+//     // const migratedContent = migrateResumeContent(resumeContent);
+//     const basePath = path.join(__dirname, '../');
+//     const pdfFilePath = path.join(
+//       basePath,
+//       path.dirname(filePath),
+//       path.basename(filePath, path.extname(filePath)) + '.pdf',
+//     );
+//     await renderToFile(
+//       <ResumeDocument resume={resumeContent} resumeDesign={DEFAULT_RESUME_DESIGN} />,
+//       pdfFilePath,
+//     );
 
-    await pdf2png(pdfFilePath);
+//     await pdf2png(pdfFilePath);
 
-    console.log(`Successfully rendered pdf: ${filePath}`);
-  } catch (error) {
-    console.error(`Error processing file ${filePath}:`, error);
-  }
-}
+//     console.log(`Successfully rendered pdf: ${filePath}`);
+//   } catch (error) {
+//     console.error(`Error processing file ${filePath}:`, error);
+//   }
+// }
 
-(async () => {
-  const targetPath = process.argv[2];
-  if (!targetPath) {
-    console.error('Please provide a file or directory path');
-    process.exit(1);
-  }
+// (async () => {
+//   const targetPath = process.argv[2];
+//   if (!targetPath) {
+//     console.error('Please provide a file or directory path');
+//     process.exit(1);
+//   }
 
-  const files = await getDirectoryFiles(targetPath, '.json');
-  files.forEach(async file => {
-    await processFile(file);
-  });
-})();
+//   const files = await getDirectoryFiles(targetPath, '.json');
+//   files.forEach(async file => {
+//     await processFile(file);
+//   });
+// })();
